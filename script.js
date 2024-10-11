bd8a7095ac901d0b5c1ef7c9d4ef1642a43ba8b5
luismellow01 <luis.melo@solidaridadnetwork.org>
2024-10-11 14:18:27 -0300

    Change basemap

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //https://leaflet-extras.github.io/leaflet-providers/preview/
 var map = L.map('map').setView([-4.3,  -50.05], 8);
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-  attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'jpg'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
 }).addTo(map);
 
 
