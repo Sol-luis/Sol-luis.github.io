ca09051f722d7f7e7d747aad786ad0d3c63a4503
luismellow01 <luis.melo@solidaridadnetwork.org>
2024-10-11 13:41:05 -0300

    adding GeoJSON to my map

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//https://leaflet-extras.github.io/leaflet-providers/preview/
 var map = L.map('map').setView([-4.3,  -50.05], 8);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data © OpenStreetMap contributors'
 }).addTo(map);
+
+
+fetch('data/gdf_muni.geojson')
+  .then(response => response.json())  // Parse the GeoJSON file
+  .then(data => {
+    L.geoJSON(data).addTo(map);  // Add the GeoJSON layer to the map
+  })
+  .catch(error => {
+    console.error('Error loading the GeoJSON file:', error);
+  });
