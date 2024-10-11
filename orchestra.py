639dbd796834148b501ccc7a397fc5153829c9c1
luismellow01 <luis.melo@solidaridadnetwork.org>
2024-10-11 12:47:37 -0300

    adicionando controle de arquivos p/ entrada e limites para teste

---
+++ orchestra.py
@@ -0,0 +1,26 @@
+import os
+import geopandas as gpd
+import pandas as pd
+
+def file_opener(): # do colab
+    return
+
+def car_homogeneo():
+    return
+
+def car_to_limit(): # pega uma lista x de cars e encontra shapefiles correspondentes
+    return
+
+def limit_to_data_folder():
+    return
+
+### Abrindo arquivos xlsx e shapefile
+
+
+### Coluna CAR dos dois bancos no mesmo tipo e formato
+
+
+### encontrando correspondências
+
+
+### Exportando resultados para pasta correspondente
