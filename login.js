1dc263f76493333add566159c0c0519d2198bddf
luismellow <luismellow01@gmail.com>
2024-12-06 10:35:30 -0300

    Add login users - change font logic

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 // login.js
-const validUsername = "admin"; 
-const validPassword = "soledemais"; 
+const validCredentials = [
+    { username: "admin", password: "soledemais" },
+    { username: "Sucafina", password: "sucafinaesol" }
+]; 
 
 document.getElementById("loginForm").addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent form submission
@@ -8,11 +10,14 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
     const inputUsername = document.getElementById("username").value;
     const inputPassword = document.getElementById("password").value;
 
-   // Check if both username and password are correct
-   if (inputUsername === validUsername && inputPassword === validPassword) {
-    // Redirect to your main page (e.g., main.html)
-    window.location.href = "main.html";
-} else {
-    alert("Usuário ou senha estão incorretos");
-}
+    const isValidUser = validCredentials.some(
+        credentials => credentials.username === inputUsername && credentials.password === inputPassword
+    );
+
+    if (isValidUser) {
+        // Redirect to your main page (e.g., main.html)
+        window.location.href = "map.html";
+    } else {
+        alert("Usuário ou senha estão incorretos");
+    }
 });
